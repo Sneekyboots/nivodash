b239a8a445aaeb111344f1341c719fcf2b598905
SneekyBoots <sneekyboots122@gmail.com>
2024-05-20 00:06:37 +0530

    Working db

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
 import globalReducer from "state";
 import { Provider } from "react-redux";
-import { setupListeners } from "@reduxjs/toolkit/query/react";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "state/api";
-
-
 
 const store = configureStore({
   reducer: {
@@ -18,9 +16,6 @@ const store = configureStore({
   middleware: (getDefault) => getDefault().concat(api.middleware),
 });
 setupListeners(store.dispatch);
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/state/index.js
+++ src/state/index.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState={
     mode :"dark",
-    userId:"63701cc1f03239bef0000123"
+    userId:"63701cc1f03239c72c00017f"
 };
 
 export const globalSlice = createSlice({
